8734b04192876c0b57c1dcf6379cd694cebf9bb2
gnas01 <gnasiopoulos@outlook.com>
2024-01-01 22:13:40 +0200

    add modal in DashboardUnits

--- src/views/DashboardUnits/DashboardUnits.tsx
+++ src/views/DashboardUnits/DashboardUnits.tsx
@@ -1,34 +1,42 @@
 /** @jsxImportSource @emotion/react */
 
 import { LinearProgress, Stack } from "@mui/material";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useQuery } from "react-query";
 import queryKeys from "../../constants/queryKeys";
 import { DashboardLayout } from "../../layouts/DashboardLayout/DashboardLayout";
-import { Unit } from "./components/Unit";
+import { AddUnitButton } from "./components/AddUnitButton/AddUnitButton";
+import { AddUnitModal } from "./components/AddUnitModal/AddUnitModal";
+import { Unit } from "./components/Unit/Unit";
 import { getUnits } from "./dashboardUnitsApi";
 import { dashboardUnitsStyles } from "./dashboardUnitsStyles";
 
 export const DashboardUnits: FC = () =>
 {
-    const { data: unitsData, isLoading: isGetUnitsLoading } = useQuery(queryKeys.units.units, getUnits, {
+    const { data: unitsData, isLoading: isGetUnitsLoading } = useQuery(queryKeys.units.getUnits, getUnits, {
         refetchInterval: 2000,
         select: responseData => responseData?.data
     });
 
+    const [isAddUnitModalOpen, setAddUnitModalOpen] = useState(false);
+
     return (
-        <DashboardLayout headerText="Units">
-            <div css={dashboardUnitsStyles}>
-                {isGetUnitsLoading ? (
-                    <Stack sx={{ width: '97%', color: 'grey.500' }} spacing={2}>
-                        <LinearProgress color="inherit" />
-                    </Stack>
-                ) : (
-                    unitsData && (unitsData.map((unit, index) => (
-                        <Unit key={index} unit={unit} />
-                    )))
-                )}
-            </div>
-        </DashboardLayout >
+        <>
+            <DashboardLayout headerText="Units" headerRightElement={<AddUnitButton onClick={() => setAddUnitModalOpen(true)} />}>
+                <div css={dashboardUnitsStyles}>
+                    {isGetUnitsLoading ? (
+                        <Stack sx={{ width: '97%', color: 'grey.500' }} spacing={2}>
+                            <LinearProgress color="inherit" />
+                        </Stack>
+                    ) : (
+                        unitsData && (unitsData.map((unit, index) => (
+                            <Unit key={index} unit={unit} />
+                        )))
+                    )}
+                </div>
+            </DashboardLayout >
+
+            <AddUnitModal isOpen={isAddUnitModalOpen} handleClose={() => setAddUnitModalOpen(false)} />
+        </>
     );
 };
