68284e7bf657d6ce7a531b6c289687c132f7f1b9
gnas01 <gnasiopoulos@outlook.com>
2024-01-01 22:15:05 +0200

    add better error handling

--- src/utils/axiosClient.ts
+++ src/utils/axiosClient.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import authService from '../services/authService';
+import { ApiError } from '../types/apiError';
 import { refresh } from '../views/Login/loginApi';
 
 const axiosClient = axios.create({
@@ -63,5 +64,26 @@ axiosClient.interceptors.response.use(
         return Promise.reject(error);
     }
 );
+
+const isApiError = (error: unknown): error is ApiError => 
+{
+    return (error as ApiError).message !== undefined && (error as ApiError).statusCode !== undefined && (error as ApiError).errorCode !== undefined;
+}
+
+axiosClient.interceptors.response.use(null, error => {
+    if (axios.isAxiosError(error) && error.response) 
+    {
+        console.log(error.response);
+        const apiError = error.response.data as ApiError;
+        apiError.statusCode = error.response.status;
+
+        if(isApiError(apiError)) 
+        {
+            return Promise.reject(apiError);
+        }
+
+        return Promise.reject(error);
+    }
+})
 
 export default axiosClient;
