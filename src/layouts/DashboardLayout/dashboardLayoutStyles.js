3f7a3c27c054253f10ef197c84e6861b0db89892
gnas01 <gnasiopoulos@outlook.com>
2024-01-07 02:18:17 +0200

    fix dashboard header z-index issue

--- src/layouts/DashboardLayout/dashboardLayoutStyles.js
+++ src/layouts/DashboardLayout/dashboardLayoutStyles.js
@@ -14,26 +14,28 @@ export const mainContent = () => css`
 
     .header-container {
         position: fixed;
-        
         display: flex;
         flex-direction: row;
         justify-content: space-between; /* aligns children to the start and end of container */
 
         background-color: #EBEBEB;
         max-width: calc(100% - 14rem);
-        width: 100%;
+        min-width: 100%;
         box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.2);
+
+        .header-text {
+            margin-inline-start: 2rem;
+            padding: 2rem 0 2rem;
+        }
+
+        .header-right-element {
+            margin-inline-end: 2rem;
+            padding: 2rem 0 2rem;
+        }
+        
+        z-index: 10000;
     }
 
-    .header-text {
-        margin-inline-start: 2rem;
-        padding: 2rem 0 2rem;
-    }
-
-    .header-right-element {
-        margin-inline-end: 2rem;
-        padding: 2rem 0 2rem;
-    }
 
     .body {
         padding-top: 7rem;
