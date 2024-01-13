14a542a4d0f65da0f2b69e414b262eec5f1e4336
gnas01 <gnasiopoulos@outlook.com>
2024-01-13 18:09:10 +0200

    simplify dashboard settings tabs by using material ui tabs

--- src/views/DashboardSettings/DashboardSettings.tsx
+++ src/views/DashboardSettings/DashboardSettings.tsx
@@ -1,62 +1,43 @@
 import React, { useState } from 'react';
-import { Tabs, Tab, Box } from '@mui/material';
+import { Tab, Box } from '@mui/material';
 import { DashboardLayout } from '../../layouts/DashboardLayout/DashboardLayout';
 import { AlarmSettingsTab } from './tabs/AlarmSettingsTab';
 import { PingerSettingsTab } from './tabs/PingerSettingsTab';
-
-interface TabPanelProps
-{
-    children?: React.ReactNode;
-    index: number;
-    value: number;
-}
-
-const TabPanel: React.FC<TabPanelProps> = ({ children, value, index, ...other }) =>
-{
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box sx={{ p: 3 }}>
-                    <div>{children}</div>
-                </Box>
-            )}
-        </div>
-    );
-};
+import { TabContext, TabList, TabPanel } from '@mui/lab';
 
 const DashboardSettings: React.FC = () =>
 {
-    const [value, setValue] = useState(0);
+    const [currentTab, setCurrentTab] = useState("alarm");
 
-    const handleChange = (event: React.SyntheticEvent, newValue: number) =>
+    const handleChange = (event: React.SyntheticEvent, newValue: string) =>
     {
-        setValue(newValue);
+        setCurrentTab(newValue);
     };
 
     return (
         <DashboardLayout headerText='Settings'>
             <Box sx={{ width: '100%' }}>
-                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <Tabs value={value} onChange={handleChange}>
-                        <Tab label="Alarm" />
-                        <Tab label="Pinger" />
-                    </Tabs>
-                </Box>
+                <TabContext value={currentTab}>
 
-                <TabPanel value={value} index={0}>
-                    <AlarmSettingsTab />
-                </TabPanel>
-                <TabPanel value={value} index={1}>
-                    <PingerSettingsTab />
-                </TabPanel>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                        <TabList onChange={handleChange}>
+                            <Tab label="Alarm" value="alarm" />
+                            <Tab label="Pinger" value="pinger" />
+                        </TabList>
+                    </Box>
+
+                    <TabPanel value="alarm">
+                        <AlarmSettingsTab />
+                    </TabPanel>
+
+                    <TabPanel value="pinger">
+                        <PingerSettingsTab />
+                    </TabPanel>
+
+                </TabContext>
             </Box>
 
-        </DashboardLayout>
+        </DashboardLayout >
     );
 };
 
